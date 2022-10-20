d322858414e03ead5714af3a13b7ee3d9023ee1d
Ayoub-Tehari <ayoub.tehari01@gmail.com>
2022-10-20 21:50:18 +0200

    menu : version beta

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,73 @@
-import "./navbar.style.css";
+//import "./navbar.style.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import HomeIcon from "@mui/icons-material/Home";
 import { IconButton } from "@mui/material";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+//import {withRouter} from "react-router-dom";
 
-export default function NavBar() {
+function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClose = () => {
+    setAnchorEl(null);
+    window.location.href = '/';
+  };
+  const handleHeroes = () => {
+    setAnchorEl(null);
+    window.location.href = '/heroes';
+  };
+  const handleComics = () => {
+    setAnchorEl(null);
+    window.location.href = '/comics';
+  };
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" style={{ backgroundColor: "red" }}>
         <Toolbar>
-          <Link to={`/?`}>
-            <IconButton>
-              <HomeIcon style={{ color: "white", marginRight: "10px" }} />
-            </IconButton>
-          </Link>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+          >
+            <MenuIcon />
+
+          </IconButton>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                
+                  <MenuItem onClick={handleClose} >Home</MenuItem>
+                
+                
+                  <MenuItem onClick={handleHeroes} >Heros</MenuItem>
+                
+                
+                  <MenuItem onClick={handleComics} >Comics</MenuItem>
+                
+              </Menu>
+          
           <Typography
             className="title"
             variant="h3"
@@ -29,3 +80,4 @@ export default function NavBar() {
     </Box>
   );
 }
+export default NavBar;
