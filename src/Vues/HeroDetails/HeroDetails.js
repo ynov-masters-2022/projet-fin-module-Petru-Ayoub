254d81249a01f7540594b21ebb0c597cc3d7c8f7
Oltean <poltean33@gmail.com>
2022-11-02 14:33:21 +0100

    heroDetails inital ui

--- src/Vues/HeroDetails/HeroDetails.js
+++ src/Vues/HeroDetails/HeroDetails.js
@@ -3,15 +3,15 @@ import "./heroDetails.style.css"
 import { Link, useParams } from "react-router-dom";
 import { fetchData } from "../../Services/FetchData";
 
-const parameter = "standard_xlarge.";
+const parameter = "portrait_incredible.";
 
 export default function HeroDetails() {
   //get url parameter -> hero id
   let { id } = useParams();
   const [hero, setHero] = React.useState([]);
   const [comics, setComics] = React.useState([]);
-  const [lien, setLien] = React.useState("");
-  
+  const [image, setImage] = React.useState("");
+
   React.useEffect(() => {
     getHero();
   }, []);
@@ -19,14 +19,22 @@ export default function HeroDetails() {
   const getHero = async () => {
     const response = await fetchData("characters", id);
     setHero(response.data.results[0]);
-    setLien(response.data.results[0].thumbnail.path + "/" + parameter + response.data.results[0].thumbnail.extension);
+    setImage(response.data.results[0].thumbnail.path + "/" + parameter + response.data.results[0].thumbnail.extension);
     setComics(response.data.results[0].comics.items);
   };
-  //console.log(hero.thumbnail.path + "." + hero.thumbnail.extension);
-  return <><img src={lien}
-        alt="thumbnail"
-      /><h3>{hero.name}</h3><div className="comics-container">
-       <ul> { comics.map((comic) => <li key={comic.resourceURI}><Link to={`/comic/${comic.resourceURI.substring(comic.resourceURI.lastIndexOf('/') + 1)}`}><p>{comic.name}</p></Link> </li>)
-       }</ul>
-      </div></>;
+
+  return (
+    <div className="detailsWrapper">
+      <img className="portrait" src={image} alt="thumbnail" />
+      <h3 className="heroName">{hero.name}</h3>
+      <p>
+        {hero.description || "This Hero Has No Description"}
+      </p>
+      <div className="comicList">
+        <span>Comics thats {hero.name} appears in: </span>
+        {comics.map((comic) => <li key={comic.resourceURI}><Link to={`/comic/${comic.resourceURI.substring(comic.resourceURI.lastIndexOf('/') + 1)}`}><p>{comic.name}</p></Link> </li>)}
+      </div>
+    </div>
+
+  );
 }
